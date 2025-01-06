cc7bd97d50e8f72c344e6818120e03383c08c985
XyrelTzy <xyreltzy1111@gmail.com>
2025-01-06 17:43:17 +0800

    New Cards UI and Responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>
-    <!-- Status Not Done -->
-    <p>Dashboard Content Here</p>
+  <div class="">
+    <div class="w-full bg-white h-16">
+        <h2 class="text-2xl px-4 py-4 shadow-lg">Dashboard</h2>
+    </div>
+    <div class="w-full flex flex-1 flex-col gap-14 lg:flex-row md:flex-row mt-10 items-center flex-wrap">
+      <div class="w-auto flex-1">
+        Cards 1
+      </div>
+      <div class="w-auto flex-1">Cards 2</div>
+      <div class="w-auto flex-1">Cards 3</div>
+      <div class="w-auto flex-1">Cards 4</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style lang="scss">
+</style>
